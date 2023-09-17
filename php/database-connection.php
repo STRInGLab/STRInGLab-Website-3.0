be77120d223d519cb47a61f2bf9cd6f4c2b9a137
S.T.R.In.G <tech@stringlab.org>
2023-09-17 21:31:37 +0530

    SMS-Trigger

--- php/database-connection.php
+++ php/database-connection.php
@@ -17,3 +17,17 @@ if ($mysqli->connect_error) {
 }
 
 ?>
+
+<!-- CREATE TABLE contact_submissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    phone integer(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message TEXT,
+    page_source VARCHAR(255)
+);
+
+ALTER TABLE contact_submissions 
+ADD timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+ADD section VARCHAR(255); -->
