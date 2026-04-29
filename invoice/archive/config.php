dbf3d7e5e1d0d4ced7ce128793b21fe96a82d88b
Shikhar Mishra <tech@stringlab.org>
2026-04-30 00:02:39 +0530

    Removed unnecessary files.

--- invoice/archive/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// config.php
-$host = '82.180.152.1';
-$db   = 'u758484694_stringpricing';
-$user = 'u758484694_stringpricing'; // replace with your MySQL username
-$pass = 'Wj~7j@huK'; // replace with your MySQL password
-$charset = 'utf8mb4';
-?>
